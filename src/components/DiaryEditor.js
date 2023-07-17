0c39abfec42df44718d3455fc31c8bb6eae337ec
7p3m1k <kor0885@gmail.com>
2023-07-17 23:57:05 +0900

    add: 상세 일기 페이지 헤더 작성

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -6,6 +6,8 @@ import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
 
+import { getStringDate } from "./util/date";
+
 const emotionList = [
   {
     emotion_id: 1,
@@ -25,7 +27,7 @@ const emotionList = [
   {
     emotion_id: 4,
     emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "나쁘",
+    emotion_descript: "나쁨",
   },
   {
     emotion_id: 5,
@@ -33,10 +35,6 @@ const emotionList = [
     emotion_descript: "끔찍함",
   },
 ];
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,16 +1,54 @@
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+
+import { getStringDate } from "../components/util/date";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
 
 const Diary = () => {
   const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data, setData] = useState();
 
   console.log(id);
 
-  return (
-    <div>
-      <h1>Diary</h1>
-      <p>이곳은 일기 상세 페이지 입니다.</p>
-    </div>
-  );
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+
+      if (targetDiary) {
+        setData(targetDiary);
+      } else {
+        alert("없는 일기 입니다.");
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className="DiaryPage">로딩중입니다...</div>;
+  } else {
+    return (
+      <div className="DiaryPage">
+        <MyHeader
+          headText={`${getStringDate(new Date(data.date))} 기록`}
+          leftChild={
+            <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+          }
+          righthChild={
+            <MyButton
+              text={"수정하기"}
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
+        />
+      </div>
+    );
+  }
 };
 
 export default Diary;
